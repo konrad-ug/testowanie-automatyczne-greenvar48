1ea74282bda24e058e2c62b759eeb05494d6b9df
greenvar48 <jackie2018@protonmail.com>
2022-10-21 15:30:22 +0200

    Features 2 to 4

--- app/Konto.py
+++ app/Konto.py
@@ -1,5 +1,16 @@
+import re
+
 class Konto:
-    def __init__(self, imie, nazwisko):
+    def __init__(self, imie, nazwisko, pesel, kod_rabatowy=None):
         self.imie = imie
         self.nazwisko = nazwisko
-        self.saldo = 0
+
+        if len(pesel) == 11:
+            self.pesel = pesel
+        else:
+            self.pesel = "Niepoprawny pesel!"
+        
+        if kod_rabatowy is not None and re.match("^PROMO_\S{3}$", kod_rabatowy):
+            self.saldo = 50
+        else:
+            self.saldo = 0
